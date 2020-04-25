e9a38f3ac473ad23cfec5ef89fcffa514b5a3b3e
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-25 00:57:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,9 +15,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
-  array 
+  array.sort_by {|idx| idx[2] <=> idx[0]}
 end 
+
 
 def reverse_array(array)
   array.reverse
